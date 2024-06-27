1645efd2c136073df3626555e8ee5532a7eb92dd
mizukamiskr <dzy32625@gmail.com>
2024-06-27 09:43:49 +0900

    1

---
+++ Block.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "GameObject.h"
+
+#define BLOCK_SIZE 8
+
+//ブロックタイプ
+enum BlockType {
+    STONE,
+    WOOD,
+    BRICK,
+    PUMPKIN,
+};
+
+class Block :
+    public GameObject
+{
+public:
+    BlockType blockType = BlockType::STONE;
+
+    Block() = default;
+    ~Block() = default;
+};
+
